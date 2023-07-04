79345e7f692286391d775e50bbcbd37494223673
Hank <hank-c@qq.com>
2023-07-04 18:00:40 +0800

    remove dirty code

--- src/native/clipboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIPBOARD_H
-#define CLIPBOARD_H
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> ReadFilePaths();
-
-#endif
--- src/native/test.js
+++
@@ -1,3 +0,0 @@
-let native = require("./build/Release/addon.node");
-let paths = native.readFilePaths();
-console.log(paths);
